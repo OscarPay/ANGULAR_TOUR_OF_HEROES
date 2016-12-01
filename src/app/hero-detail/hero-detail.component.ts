ed488399585b92b662355a0165a92f09f1f27da6
Oscar <operez@kwantec.com>
2016-12-01 08:11:31 -0600

    Routing and styles finished

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, Input  } from '@angular/core';
-import { Hero } from '.././hero';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params}   from '@angular/router';
+import {Location}                 from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import {HeroService} from '.././hero.service';
+import {Hero} from '.././hero';
 
 @Component({
   selector: 'my-hero-detail',
@@ -8,12 +13,21 @@ import { Hero } from '.././hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input()
   hero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService,
+              private route: ActivatedRoute,
+              private location: Location) {
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
