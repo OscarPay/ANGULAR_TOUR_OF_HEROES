e59902e1f760dc4b4045c9cbb0f5a3137978df76
Oscar <operez@kwantec.com>
2016-12-01 23:08:40 -0600

    Ahora tengo el CRUD para los heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,11 @@ import {HeroesComponent} from './heroes/heroes.component';
 import {HeroService} from './hero.service';
 import {DashboardComponent} from './dashboard/dashboard.component'
 
-import { AppRoutingModule }     from './app-routing.module';
+// Imports for loading & configuring the in-memory web api
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService}  from './in-memory-data.service';
+
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -22,6 +26,7 @@ import { AppRoutingModule }     from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
