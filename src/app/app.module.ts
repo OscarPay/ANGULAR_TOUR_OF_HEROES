f74d91cd7bb79fddef1d6d8c20a95f459c1a20c8
Oscar <operez@kwantec.com>
2016-12-01 23:30:00 -0600

    Se agrego el search button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,17 @@ import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService}  from './in-memory-data.service';
 
 import {AppRoutingModule} from './app-routing.module';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+import './rxjs-extensions';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
